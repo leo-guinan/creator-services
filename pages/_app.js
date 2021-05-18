f8e395fe916777987e653847a9002b6a9e60e88c
leo-guinan <leo@guinantech.com>
2021-05-17 23:01:38 -0400

    setting up initial site based on tutorial

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import { Amplify } from "aws-amplify";
+import { AmplifyAuthenticator } from "@aws-amplify/ui-react";
+import awsExports from "../src/aws-exports";
+import "../styles/globals.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+Amplify.configure({ ...awsExports, ssr: true });
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <AmplifyAuthenticator>
+      <Component {...pageProps} />
+    </AmplifyAuthenticator>
+  );
 }
-
-export default MyApp
+export default MyApp;
